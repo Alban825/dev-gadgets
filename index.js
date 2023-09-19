9f53a654b645089a3856e73e827f68d2ff2d9269
Alban Colibert <albancolibert825@gmail.com>
2023-09-19 11:16:02 +0200

    button-right-left

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const prevButton = document.querySelector('.pictures-prev');
-const pictures = document.querySelector('.pictures-img');
-const thumbnailImages = document.querySelectorAll('.thumbs-img');
-const imageSources = [
-    'img/canard-jaune-1-l.png',
-    'img/canard-jaune-2-l.png',
-    'img/canard-jaune-3-l.png',
-    'img/canard-jaune-4-l.png',
-    'img/canard-jaune-5-l.png'
-];
+document.addEventListener("DOMContentLoaded", function () {
+    const previousButton = document.getElementById("prevButton");
+    const nextButton = document.getElementById("nextButton");
+    const image = document.getElementById("image");
+    const imageSources = [
+      "img/canard-jaune-1-l.png",
+      "img/canard-jaune-2-l.png",
+      "img/canard-jaune-3-l.png",
+      "img/canard-jaune-4-l.png",
+      "img/canard-jaune-5-l.png",
+    ];
+    let currentIndex = 0;
+  
+    function showImage(index) {
+      image.src = imageSources[index];
+    }
+  
+    function showNextImage() {
+      currentIndex = (currentIndex + 1) % imageSources.length;
+      showImage(currentIndex);
+    }
+  
+    function showPrevImage() {
+      currentIndex = (currentIndex - 1 + imageSources.length) % imageSources.length;
+      showImage(currentIndex);
+    }
+  
+    nextButton.addEventListener("click", showNextImage);
+  
+    previousButton.addEventListener("click", showPrevImage);
+  
 
-let currentImageIndex = 0;
-prevButton;addEventListener('click', () => {
-    currentImageIndex = (currentImageIndex - 1 + imageSources.length) % imageSources.length;
-    pictures.src = imageSources[currentImageIndex];
-    thumbnailImages[currentImageIndex].classList.add('active');
-    thumbnailImages.forEach((image, index) => {
-        if (index !== currentImageIndex)  {
-            image.classList.remove('active');
-        }
-    });
-});		
-    
+    showImage(currentIndex);
+  });
+  
